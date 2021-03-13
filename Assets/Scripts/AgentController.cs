db65c194433cb59b4c00b14414026eaf39581441
Alex Semchenkov <43003303+Provohito@users.noreply.github.com>
2021-03-13 11:02:23 +0300

    Initial commit

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AgentController : MonoBehaviour
+{
+    [SerializeField]
+    Camera mainCamera;
+
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+                agent.SetDestination(hit.point);
+        }
+    }
+}
